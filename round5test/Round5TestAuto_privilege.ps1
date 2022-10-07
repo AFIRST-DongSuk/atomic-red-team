5b0e356b423feaf7464df929291a12f223cd742f
mihee.seo <afirst.smh@gmail.com>
2022-10-07 17:12:38 +0900

    Add auto test script   Round5TestAuto_privilege.ps1   Roun5TestAuto.ps1 Rename file name   Round5TestStep(amin).ps1 --> Round5TestStep_privilege.ps1   Round5TestStep(amin)_pause.ps1 --> Round5TestStep_privilege_pause.ps1

---
+++ round5test/Round5TestAuto_privilege.ps1
@@ -0,0 +1,37 @@
+#powershell -exec bypass
+
+# if dir exist 
+# do nothing
+# else
+#IEX (IWR 'https://raw.githubusercontent.com/crav3r/invoke-atomicredteam/round4test/install-atomicredteam.ps1' -UseBasicParsing);
+#Install-AtomicRedTeam -getAtomics -getRound4TestStep -InstallPath "C:\Mitre\Round5" -Force
+
+Import-Module "C:\Mitre\Round5\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1" -Force
+
+$PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="C:\Mitre\Round5\atomic-red-team\atomics"}
+$ExecutionLogPath = "C:\Mitre\Round5\atomic-red-team\atomictest_auto_privilege.log"
+$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).isInRole([Security.Principal.WindowsBuiltinRole]::Administrator) 
+
+
+Start-Transcript "C:\Mitre\Round5\Round5TestAuto_privilege.ps1.log"
+
+$sleeptime = 10
+
+$file = Get-Content C:\Mitre\Round5\atomic-red-team\round5test\Round5TestStep_privilege.ps1
+
+foreach ($line in $file)
+{
+    $dt=date
+    if($line -like 'invoke-atomictest*')
+    {
+        write-host $dt.ToString("yyyy-MM-dd HH:mm:ss");
+        IEX $line
+        timeout /t $sleeptime
+    }
+    else {
+        #do nothing
+    }
+    
+}
+
+Stop-Transcript
