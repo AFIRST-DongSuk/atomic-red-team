b9be02b25ed830bb6ec31dd5fa20131c52a92c38
hyunwoo.jung <gusdn8408@gmail.com>
2022-11-01 16:51:11 +0900

    modify

--- round5test/Round5TestAuto_privilege.ps1
+++ round5test/Round5TestAuto_privilege.ps1
@@ -6,18 +6,18 @@
 #IEX (IWR 'https://raw.githubusercontent.com/crav3r/invoke-atomicredteam/round4test/install-atomicredteam.ps1' -UseBasicParsing);
 #Install-AtomicRedTeam -getAtomics -getRound4TestStep -InstallPath "C:\Mitre\Round5" -Force
 
-Import-Module "C:\r5\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1" -Force
+Import-Module "C:\round5test\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1" -Force
 
-$PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="C:\r5\atomic-red-team\atomics"}
-$ExecutionLogPath = "C:\r5\atomic-red-team\atomictest_auto_privilege.log"
+$PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="c:\round5test\atomics"}
+$ExecutionLogPath = "c:\round5test\atomictest_auto_privilege.log"
 $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).isInRole([Security.Principal.WindowsBuiltinRole]::Administrator) 
 
 
-Start-Transcript "C:\r5\Round5TestAuto_privilege.ps1.log"
+Start-Transcript "c:\round5test\Round5TestAuto_privilege.ps1.log"
 
 $sleeptime = 10
 
-$file = Get-Content C:\r5\atomic-red-team\round5test\Round5TestStep_privilege.ps1
+$file = Get-Content c:\round5test\Round5TestStep_privilege.ps1
 
 foreach ($line in $file)
 {
