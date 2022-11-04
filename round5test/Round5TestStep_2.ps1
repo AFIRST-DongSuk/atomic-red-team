28dce31209af08e44fbfd5297759e26b4683c47b
seunghak.kim <afirst.ksh@gmail.com>
2022-11-04 14:02:37 +0900

    modify

---
+++ round5test/Round5TestStep_2.ps1
@@ -0,0 +1,11 @@
+$PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="c:\round5test\atomics"}
+$ExecutionLogPath = "c:\round5test\atomictest.log"
+Import-Module "c:\round5test\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1" -Force
+
+Start-Transcript "c:\round5test\Round5TestStep_2_ps1.log"
+
+#---------------------------------------------------------------------------------------
+# T1001.002 Data Obfuscation: Steganography
+#---------------------------------------------------------------------------------------
+# N/A
+#---------------------------------------------------------------------------------------
